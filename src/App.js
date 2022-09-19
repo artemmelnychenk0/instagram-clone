95831e05e5194b7e0b9b14614db3317a7a6386b5
artem <amelnychenko06@gmail.com>
2022-09-19 11:46:02 +0200

    Minor changes were made

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import SignIn from './Components/Authentication/Authentication';
+import Home from './Components/Home/Home';
+import Messages from './Components/Messages/Messages';
+import Profile from './Components/Profile/Profile';
+import Navigation from './Components/Nav/Nav';
+import Post from './Components/Post/Post';
+import { useState } from 'react';
 
 function App() {
+
+  const [post, setPost] = useState()
+
+  const toPost = (current) => {
+    setPost(current)
+  }
+
   return (
-    <div className="App">
-      Instagram Clone
-    </div>
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/profile' element={<Profile toPost={toPost} />} />
+        <Route path='/messages' element={<Messages />} />
+        <Route path='/signup' element={<SignIn />} />
+        <Route path='/post/:id' element={<Post post={post} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
