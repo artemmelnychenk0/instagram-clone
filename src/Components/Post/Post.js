222f8cdff2294d2a42d9cc4213221e11a649c05c
artem <amelnychenko06@gmail.com>
2022-10-11 20:17:12 -0400

    Added option for user to comment

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,26 +1,45 @@
-import { collection, deleteDoc, doc, getDoc, getDocs, query, updateDoc, where } from "firebase/firestore"
+/* eslint-disable react-hooks/exhaustive-deps */
+import { arrayUnion, deleteDoc, doc, getDoc, updateDoc } from "firebase/firestore"
 import { useEffect, useState } from "react"
 import { db } from "../Data/firebase-config"
+import { auth } from "../Data/firebase-config";
 
 const Post = ({ post }) => {
 
-    const [posts, setPosts] = useState()
+    const [posts, setPosts] = useState([])
+    const [comment, setComment] = useState('')
+
+    const [postLoaded, setPostLoaded] = useState(false)
 
     useEffect(() => {
         getPosts()
     }, [])
 
     const getPosts = async () => {
-        const docRef = doc(db, 'posts', `${post}`)
-        const docSnap = await getDoc(docRef)
-        // const q = query(collection(db, "posts"), where("time", "==", post));
-        // const data = await getDocs(q);
-        console.log(docSnap.data())
+        const docRef = doc(db, 'posts', `${post}`);
+        const docSnap = await getDoc(docRef);
         setPosts(docSnap.data());
-        // setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+        setPostLoaded(true)
+    }
+
+    const addComment = async (event) => {
+        const timestamp = Date.now();
+        event.preventDefault()
+        const docRef = doc(db, 'posts', `${post}`)
+        await updateDoc(docRef,
+            {
+                comments: arrayUnion({
+                    comment: `${comment}`,
+                    time: `${timestamp}`,
+                    user: auth.currentUser.displayName
+                })
+            }
+        )
+
     }
 
     const updateLikes = async (id, likes) => {
+        console.log(id)
         const userDoc = doc(db, 'posts', id);
         const likeIncr = { likes: likes + 1 };
         await updateDoc(userDoc, likeIncr)
@@ -31,31 +50,46 @@ const Post = ({ post }) => {
         await deleteDoc(userDoc)
     }
 
-    const addComment = async (id) => {
-    }
-
     return (
         <div>
-            <div>Post</div>
-            <div>{post}</div>
+            {postLoaded &&
+                <div key={posts.id} className='single'>
+                    <h1>Name: {posts.name}</h1>
+                    <h1>Post: {posts.caption}</h1>
+                    <img src={posts.imageUrl} alt='imagePost' />
+                    <h1>Likes: {posts.likes}</h1>
 
-            <div key={posts.id} className='single'>
-                <h1>Name: {posts.name}</h1>
-                <h1>Post: {posts.caption}</h1>
-                <img src={posts.imageUrl} alt='imagePost' />
-                <h1>Likes: {posts.likes}</h1>
-                <h2>Comment: {posts.comments[1]}</h2>
-                <button
-                    onClick={() => {
-                        updateLikes(posts.id, posts.likes)
-                    }}>Like
-                </button>
-                <button onClick={() => {
-                    deletePost(post.id)
-                }}>
-                    Delete
-                </button>
-            </div>
+                    <div>
+                        {posts.comments.map(comment => {
+                            return (
+                                <div key={comment.time}>
+                                    Comment by: {comment.user}
+                                    <br></br>
+                                    {comment.comment}
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <button
+                        onClick={() => {
+                            updateLikes(post, posts.likes)
+                        }}>Like
+                    </button>
+                    <button onClick={() => {
+                        deletePost(post)
+                    }}>
+                        Delete
+                    </button>
+                    <div>
+                        <form onSubmit={addComment}>
+                            <h2>Add comment</h2>
+                            <textarea type='text' placeholder="Caption" onChange={(e) => { setComment(e.target.value) }} />
+                            <br></br>
+                            <button>Create</button>
+                        </form>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
