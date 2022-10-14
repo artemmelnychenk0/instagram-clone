8d68b5309ba600c28f133500306bb994c9289583
artem <amelnychenko06@gmail.com>
2022-10-14 17:19:22 -0400

    Changed sign in and sign out feature

--- src/Components/Authentication/Authentication.js
+++ src/Components/Authentication/Authentication.js
@@ -1,46 +1,66 @@
 
 import { useState } from "react";
-import { auth } from "../Data/firebase-config";
+import { auth, db } from "../Data/firebase-config";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { signOut } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
 
 const SignIn = () => {
 
-    const [username, setUsername] = useState('')
-    const [userinput, setUserInput] = useState('')
+    const [userInput, setUserInput] = useState('')
 
     const navigate = useNavigate()
 
     const changeHandler = (e) => {
         setUserInput(e.target.value)
     }
-    const handleSubmit = async () => {
+    const login = async () => {
+
         const provider = new GoogleAuthProvider();
         await signInWithPopup(auth, provider)
             .then(() => {
                 navigate('/profile')
-
             })
             .catch((err) => {
                 console.log(err)
             })
     }
 
-    const singOutUser = () => {
-        signOut(auth)
-            .then(() => {
+    const signUp = async (e) => {
+        e.preventDefault()
+
+        const provider = new GoogleAuthProvider();
+        await signInWithPopup(auth, provider)
+            .then((result) => {
+                const user = result.user
+                const userId = user.reloadUserInfo.localId
+                setDoc(doc(db, 'users', `${userId}`), {
+                    email: user.email,
+                    followers: [],
+                    folowing: [],
+                    bio: `Hi my name is ${user.displayName}`,
+                    username: userInput
+                })
+                navigate('/profile')
+            })
+            .catch((err) => {
+                console.log(err)
             })
     }
+
 
 
     return (
         <div>
             <h3>Sign Up</h3>
-            <input type='text' placeholder="username" onChange={changeHandler}></input>
-            <button onClick={handleSubmit}>Sign In With Google</button>
+            <form onSubmit={signUp}>
+
+                <input type='text' placeholder='username' required minLength='3' onChange={changeHandler} name='username'></input>
+                <br></br>
+                <button>Sign Up With Google</button>
+            </form>
             <p> Already signed up?</p>
-            <button onClick={singOutUser}>Sign Out</button>
+            <button onClick={login}>Login</button>
         </div>
     )
 }
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -2,8 +2,38 @@ import { Link } from "react-router-dom"
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { auth } from "../Data/firebase-config";
+import { useEffect, useState } from "react";
+import { signOut } from "firebase/auth";
 
 const Navigation = () => {
+
+    const [userSigned, setUserSigned] = useState(true)
+
+    useEffect(() => {
+        onAuthStateChange()
+    }, [])
+
+    const singOutUser = () => {
+        signOut(auth)
+            .then(() => {
+                setUserSigned(true)
+            })
+    }
+
+    const onAuthStateChange = () => {
+        return auth.onAuthStateChanged(user => {
+            if (user) {
+
+                setUserSigned(false)
+
+            } else {
+
+                setUserSigned(true)
+            }
+        })
+    }
+
     return (
         <Navbar sticky="top" bg="dark" variant="dark" >
             <Container>
@@ -11,7 +41,12 @@ const Navigation = () => {
                 <Nav className="justify-content-center">
                     <Link to="/profile" className="nav-link">Profile</Link>
                     <Link to="/home" className="nav-link">Home</Link>
-                    <Link to="/signUp" className="nav-link">SignUp</Link>
+                    {userSigned &&
+                        <Link to="/signUp" className="nav-link">SignUp</Link>
+                    }
+                    {!userSigned &&
+                        <Link to="/home" className="nav-link" onClick={singOutUser}>SignOut</Link>
+                    }
                 </Nav>
             </Container>
         </Navbar>
